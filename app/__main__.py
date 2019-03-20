6acb309d6b9561bcd096a65fd5ed7a557ac744ef
carkod <carkodw@gmail.com>
2019-03-20 21:33:08 +0100

    new conda env

--- app/__main__.py
+++ app/__main__.py
@@ -4,7 +4,7 @@
 #%%
 import get_data
 import candlestick
-# df = get_data.get_data()
-df = get_data.static_data()
+df = get_data.get_data()
+# df = get_data.static_data()
 
 candlestick.candlestick_plot(df)
