67c6c3f86ea305f5750159313789265acea5b106
carkod <carkodw@gmail.com>
2019-03-30 16:19:34 +0100

    macd finished required testing

--- app/algorithms/sudden_inc.py
+++ app/algorithms/sudden_inc.py
@@ -1,15 +1,16 @@
 # %%
 import sys
-sys.path.append('D:\\algobin-notebook')
+# sys.path.append('D:\\algobin-notebook')
+sys.path.append('C:\\Users\\Carlos-Lenovo\\algobin')
 
 import pandas as pd
 import numpy as np
 from app.utilities.get_data import Data
-from app.utilities.indicators import bollinger_bands, moving_average
+from app.utilities.indicators import bollinger_bands, moving_average, macd
 from app.utilities.api import EnumDefinitions
 
 #%% 
-def __init__(self):
+def __init__(self, symbol, interval):
     """ Sudden increase standard    
         - Bollinger bands
         - MACD
@@ -17,8 +18,34 @@ def __init__(self):
         - Checks small periods (5m, 15m, 30m)
         Indicators are used to detect buy signal
     """
-    self.interval = EnumDefinitions.chart_intervals[2:5]
-    self.time_period = '30m'
+    self.symbol = symbol
+    self.interval = interval
+
+#%%
+# def render_bb():
+#     gd = Data(interval='15m',symbol='NEOBNB')
+#     df = gd.api_data()
+#     # df = gd.static_data()
+#     bb = bollinger_bands(df, 20)
+#     m = macd(df, 25, 12)
+#     new_df = pd.concat([df, bb], sort=False)
+#     new_df.dropna(inplace=True)
+#     new_df = df.drop(['Volume', 'Quote asset volume', 'Number of trades','Taker buy base asset volume', 'Taker buy quote asset volume'], axis=1, inplace=True)
+#     return new_df
+
+
+#%%
+def render_macd():
+    gd = Data(interval='15m',symbol='NEOBNB')
+    df = gd.api_data()
+    # df = gd.static_data()
+    df.drop(['Volume', 'Quote asset volume', 'Number of trades','Taker buy base asset volume', 'Taker buy quote asset volume'], axis=1, inplace=True)
+    m = macd(df, 25, 12)
+    new_df = pd.concat([df, m], sort=False)
+    new_df.dropna(inplace=True)
+    new_df.tail()
+    return new_df
+
 
 #%%
 
@@ -29,21 +56,11 @@ def __init__(self):
 #     show(candlestick.candlestick_plot(df))
 
 #%%
-def obtain_data():
-    gd = Data(interval='15m',symbol='NEOBNB')
-    df = gd.api_data()
-    # df = gd.static_data()
-    bb = bollinger_bands(df, 20)
-    new_df = pd.concat([df, bb], sort=False)
-    new_df.dropna(inplace=True)
-    new_df.drop(['Volume', 'Quote asset volume', 'Number of trades','Taker buy base asset volume', 'Taker buy quote asset volume'], axis=1, inplace=True)
-    return new_df
-
-#%%
 def trend_signal():
+    # Bollinger bands for trend signal in this case
     # Green candle higher than Upper bollinger
     # Last 4 values are true
-    new_df = obtain_data()
+    new_df = render_bb()
     last4_df = new_df.tail(4)
     last4_df.drop(['Low', 'High', 'Open time'], axis=1, inplace=True)
     # If close price is higher than upper BB 4 times - buy
@@ -53,8 +70,25 @@ def trend_signal():
     coordinates = last4_df.values[-1].tolist()
     return diff_close_open.all()
 
-#%%
-# trend_signal()
 
+#%%
+def oscillator_signal():
+    # MACD for oscillator signal
+    # Green candle higher than Upper bollinger
+    # Last 4 values are true
+    new_df = render_macd()
+    last4_df = new_df.tail(4)
+    last4_df.drop(['Low', 'High', 'Open time'], axis=1, inplace=True)
+    last4_df.tail()
+    # If MACD diff line is higher than Signal line in the last 4 instances = buy
+    diff_macd_signal = last4_df["MACDdiff_25_12"] > last4_df["MACDsign_25_12"]
+    diff_macd_signal
+    notification_text = 'MACD indicates Strong upward trend for 30minute period'
+    coordinates = last4_df.values[-1].tolist()
+    diff_macd_signal.all()
+    return diff_macd_signal.all()
+
+#%%
+print(render_macd().tail())
 
 #%%
