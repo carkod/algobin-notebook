2d0edfa93a64a25861061dc4a0794e1d5de220b0
carkod <carkodw@gmail.com>
2019-03-31 04:28:13 +0200

    finished sudden inc algo

--- app/algorithms/sudden_inc.py
+++ app/algorithms/sudden_inc.py
@@ -9,8 +9,8 @@ from app.utilities.get_data import Data
 from app.utilities.indicators import bollinger_bands, moving_average, macd
 from app.utilities.api import EnumDefinitions
 
-#%% 
-def __init__(self, symbol, interval):
+#%%
+class Sudden_Inc:
     """ Sudden increase standard    
         - Bollinger bands
         - MACD
@@ -18,77 +18,69 @@ def __init__(self, symbol, interval):
         - Checks small periods (5m, 15m, 30m)
         Indicators are used to detect buy signal
     """
-    self.symbol = symbol
-    self.interval = interval
+        # df = gd.static_data()
 
-#%%
-# def render_bb():
-#     gd = Data(interval='15m',symbol='NEOBNB')
-#     df = gd.api_data()
-#     # df = gd.static_data()
-#     bb = bollinger_bands(df, 20)
-#     m = macd(df, 25, 12)
-#     new_df = pd.concat([df, bb], sort=False)
-#     new_df.dropna(inplace=True)
-#     new_df = df.drop(['Volume', 'Quote asset volume', 'Number of trades','Taker buy base asset volume', 'Taker buy quote asset volume'], axis=1, inplace=True)
-#     return new_df
+    def __init__(self, symbol, interval):
+        self.symbol = symbol
+        self.interval = interval
+        gd = Data(interval=interval,symbol=symbol)
+        self.df = gd.api_data()
 
+    def render_bb(self):
+        
+        bb = bollinger_bands(self.df, 20)
+        new_df = pd.concat([self.df, bb], sort=False)
+        new_df.dropna(inplace=True)
+        new_df.drop(['Volume', 'Quote asset volume', 'Number of trades','Taker buy base asset volume', 'Taker buy quote asset volume'], axis=1, inplace=True)
+        return new_df
 
-#%%
-def render_macd():
-    gd = Data(interval='15m',symbol='NEOBNB')
-    df = gd.api_data()
-    # df = gd.static_data()
-    df.drop(['Volume', 'Quote asset volume', 'Number of trades','Taker buy base asset volume', 'Taker buy quote asset volume'], axis=1, inplace=True)
-    m = macd(df, 25, 12)
-    new_df = pd.concat([df, m], sort=False)
-    new_df.dropna(inplace=True)
-    new_df.tail()
-    return new_df
+    def render_macd(self):
+        
+        m = macd(self.df, 25, 12)
+        new_df = pd.concat([self.df, m], sort=False)
+        new_df.dropna(inplace=True)
+        new_df.drop(['Volume', 'Quote asset volume', 'Number of trades','Taker buy base asset volume', 'Taker buy quote asset volume'], axis=1, inplace=True)
+        new_df.tail()
+        return new_df
 
+    # def plot_algo(object):
+    #     """Bokeh plotting (breaks debugging)
 
-#%%
+    #     """
+    #     show(candlestick.candlestick_plot(df))
 
-# def plot_algo(object):
-#     """Bokeh plotting (breaks debugging)
+    def trend_signal(self):
+        # Bollinger bands for trend signal in this case
+        # Green candle higher than Upper bollinger
+        # Last 4 values are true
+        new_df = self.render_bb()
+        last4_df = new_df.tail(4)
+        last4_df.drop(['Low', 'High', 'Open time'], axis=1, inplace=True)
+        # If close price is higher than upper BB 4 times - buy
+        diff_close_open = last4_df['Close'] > last4_df['BollingerB_20']
+        notification_text = 'Bollinger bands indicates Strong upward trend for 30minute period'
+        coordinates = last4_df.values[-1].tolist()
+        return diff_close_open.all()
 
-#     """
-#     show(candlestick.candlestick_plot(df))
+    def oscillator_signal(self):
+        # MACD for oscillator signal
+        # Green candle higher than Upper bollinger
+        # Last 4 values are true
+        new_df = self.render_macd()
+        last4_df = new_df.tail(4)
+        last4_df.drop(['Low', 'High', 'Open time'], axis=1, inplace=True)
+        # If MACD diff line is higher than Signal line in the last 4 instances = buy
+        diff_macd_signal = last4_df["MACDdiff_25_12"] > last4_df["MACDsign_25_12"]
+        notification_text = 'MACD indicates Strong upward trend for 30minute period'
+        coordinates = last4_df.values[-1].tolist()
+        return diff_macd_signal.all()
 
-#%%
-def trend_signal():
-    # Bollinger bands for trend signal in this case
-    # Green candle higher than Upper bollinger
-    # Last 4 values are true
-    new_df = render_bb()
-    last4_df = new_df.tail(4)
-    last4_df.drop(['Low', 'High', 'Open time'], axis=1, inplace=True)
-    # If close price is higher than upper BB 4 times - buy
-    diff_close_open = last4_df['Close'] > last4_df['BollingerB_20']
-    diff_close_open
-    notification_text = 'Bollinger bands indicates Strong upward trend for 30minute period'
-    coordinates = last4_df.values[-1].tolist()
-    return diff_close_open.all()
-
-
-#%%
-def oscillator_signal():
-    # MACD for oscillator signal
-    # Green candle higher than Upper bollinger
-    # Last 4 values are true
-    new_df = render_macd()
-    last4_df = new_df.tail(4)
-    last4_df.drop(['Low', 'High', 'Open time'], axis=1, inplace=True)
-    last4_df.tail()
-    # If MACD diff line is higher than Signal line in the last 4 instances = buy
-    diff_macd_signal = last4_df["MACDdiff_25_12"] > last4_df["MACDsign_25_12"]
-    diff_macd_signal
-    notification_text = 'MACD indicates Strong upward trend for 30minute period'
-    coordinates = last4_df.values[-1].tolist()
-    diff_macd_signal.all()
-    return diff_macd_signal.all()
-
-#%%
-print(render_macd().tail())
-
-#%%
+    def oscillator_strength(self):
+        new_df = self.render_macd()
+        last4_df = new_df.tail(1)
+        last4_df.drop(['Low', 'High', 'Open time'], axis=1, inplace=True)
+        # Difference between signal and macd diff
+        diff_macd_signal = last4_df["MACDdiff_25_12"] - last4_df["MACDsign_25_12"]
+        # If diff_macd_signal positive = strong long/buying signal/increase
+        # If diff_macd_signal negative = strong short/selling signal/decrease
+        return diff_macd_signal.values[-1]
