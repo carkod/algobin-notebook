b4ddeaa898387bdcf98790de5697cbfada6e1857
carkod <carkodw@gmail.com>
2019-04-15 01:44:03 +0200

    sell algo integration with balances

--- app/utilities/environment.py
+++ app/utilities/environment.py
@@ -3,3 +3,7 @@ class API_URL:
     BINANCEAPI_TICKER24 = '/api/v1/ticker/24hr'
     BINANCEAPI_CANDLESTICK = '/api/v1/klines'
     BINANCEAPI_TICKER_PRICE = '/api/v3/ticker/price'
+    BINANCEAPI_ACCOUNT = '/api/v3/account'
+    BINANCEAPI_EXCHANGE_INFO = '/api/v1/exchangeInfo'
+    BINANCE_KEY = 'x3wD38hF7Sr1o3f6AnF8BVdEQWp9a0UjJ4oLRVJu2F91NNKpJXGg6xazd4pW7D6b'
+    BINANCE_SECRET = 'jmiTomJmIL5O1Wov32k6ZZM7DL1IoU0uBJhoTkjDKzzcOoS0cKsl6QCGEyCBt32M'
